0bb0b5b3682f2c9ec0f460a1651995438b484fd9
Dimonik333 <32264050+Dimonik333@users.noreply.github.com>
2026-05-11 20:48:04 +0300

    Reorganize package structure and add Assembly Definitions

---
+++ Assets/StatefulEffects/External/UniTask/IShowHideEqualityCondition.cs
@@ -0,0 +1,19 @@
+﻿#if STATEFULEFFECTS_UNITASK_SUPPORT
+
+using CatCode.Events;
+
+namespace CatCode.StatefulEffects
+{
+    public sealed class IShowHideEqualityCondition : ICondition<ShowHideState>
+    {
+        private ShowHideState _targetState;
+
+        public void Init(ShowHideState targetState)
+            => _targetState = targetState;
+
+        public bool Check(ShowHideState value)
+            => value == _targetState;
+    }
+}
+
+#endif
--- Assets/StatefulEffects/External/UniTask/ShowHideAsyncExtensions.cs
+++ Assets/StatefulEffects/External/UniTask/ShowHideAsyncExtensions.cs
@@ -1,13 +1,29 @@
-﻿using CatCode.Events;
+﻿#if STATEFULEFFECTS_UNITASK_SUPPORT
+
+using CatCode.Events;
 using Cysharp.Threading.Tasks;
 using System.Threading;
+using UnityEngine.Pool;
 
 namespace CatCode.StatefulEffects
 {
     public static class ShowHideAsyncExtensions
     {
-        public static UniTask WaitStateAsync(this IShowHide showHide, ShowHideState targetState, CancellationToken token)
-            => showHide.State.WaitAsync((v) => v == targetState, true, token);
+        private readonly static ObjectPool<IShowHideEqualityCondition> s_conditionPool;
+
+        static ShowHideAsyncExtensions()
+        {
+            s_conditionPool = new ObjectPool<IShowHideEqualityCondition>(
+                createFunc: () => new(),
+                collectionCheck: false);
+        }
+
+        public static async UniTask WaitStateAsync(this IShowHide showHide, ShowHideState targetState, CancellationToken token)
+        {
+            using var handle = s_conditionPool.Get(out var condition);
+            condition.Init(targetState);
+            await showHide.State.WaitAsync(condition, true, token);
+        }
 
         public static UniTask ShowAsync(this IShowHide showHide, CancellationToken token)
         {
@@ -28,3 +44,5 @@ namespace CatCode.StatefulEffects
             => showHide.WaitStateAsync(ShowHideState.Hidden, token);
     }
 }
+
+#endif
