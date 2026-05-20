f167751bf0cdb40a6b416eb60b0d8f5647d45620
Dimonik333 <32264050+Dimonik333@users.noreply.github.com>
2026-05-20 17:45:36 +0300

    Added ShowHide effect for AlphaModifiers

---
+++ Assets/StatefulEffects/External/AlphaModifiers/Runtime/AlphaModifierShowHide.cs
@@ -0,0 +1,79 @@
+﻿using CatCode.AlphaModifiers;
+using CatCode.Events;
+using CatCode.StatefulEffects;
+using DG.Tweening;
+using System;
+using UnityEngine;
+
+public sealed class AlphaModifierShowHide : MonoShowHide
+{
+    [Serializable]
+    public struct AlphaEffectSettings
+    {
+        public float Alpha;
+        public float Duration;
+        public Ease Ease;
+        public bool UseCurve;
+        public AnimationCurve Curve;
+    }
+
+    private Tween _tween;
+    private IShowHide _showHide;
+
+    [SerializeField] private ShowHideState _initialState;
+    [SerializeField] private bool _ignoreState;
+
+    [SerializeField] private MonoAlphaModifier _alphaModifier;
+    [SerializeField] private AlphaEffectSettings _showSettings;
+    [SerializeField] private AlphaEffectSettings _hideSettings;
+    [SerializeField] private bool _isIndependentUpdate;
+
+    private void Awake()
+    {
+        _showHide = new ShowHideCallbackStateMachine(
+            _initialState,
+            _ignoreState,
+            OnShow,
+            OnHide,
+            OnSetShown,
+            OnSetHidden,
+            OnStop);
+    }
+
+    private void OnShow(Action onCompleted) => ChangeAlpha(_showSettings, onCompleted);
+    private void OnSetShown() => SetAlpha(_showSettings);
+    private void OnHide(Action onCompleted) => ChangeAlpha(_hideSettings, onCompleted);
+    private void OnSetHidden() => SetAlpha(_hideSettings);
+    private void OnStop() => _tween.Kill();
+
+    private void ChangeAlpha(AlphaEffectSettings settings, Action callback)
+    {
+        _tween.Kill();
+        _tween = DOTween
+            .To(() => _alphaModifier.Alpha, value => _alphaModifier.Alpha = value, settings.Alpha, settings.Duration)
+            .OnComplete(()=>callback());
+
+        if (settings.UseCurve)
+            _tween.SetEase(settings.Curve);
+        else
+            _tween.SetEase(settings.Ease);        
+    }
+
+    private void SetAlpha(AlphaEffectSettings settings)
+    {
+        _tween.Kill();
+        _alphaModifier.Alpha = settings.Alpha;
+    }
+
+    #region IShowHide 
+
+    public override IReadOnlyEventValue<ShowHideState> State => _showHide.State;
+
+    public override void Show() => _showHide.Show();
+    public override void SetShown() => _showHide.SetShown();
+    public override void Hide() => _showHide.Hide();
+    public override void SetHidden() => _showHide.SetHidden();
+    public override void Stop() => _showHide.Stop();
+
+    #endregion
+}
